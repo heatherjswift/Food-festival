957fcdfbf914c606f0a8268312e4eb7e20abcdf6
Heather Swift <78330855+heatherjswift@users.noreply.github.com>
2021-06-24 13:09:09 -0400

    consolidate bootstrap and jquery

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const webpack = require("webpack");
+const path = require("path");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = {
+    entry: "./assets/js/script.js",
+    output: {
+        path: path.join(__dirname + "/dist"),
+        filename: 'main.bundle.js'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+        new BundleAnalyzerPlugin({
+            // the report outputs to the HTML file in the dist folder
+            analyzerMode: "static", 
+        })
+    ],
+    mode: 'development'
+};
